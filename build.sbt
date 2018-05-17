bbb29e28c0610523a5ea9b69c0148c9afcdf3151
harry <a@nc.om>
2018-05-16 19:13:16 -0700

    adding project

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "BigDataApplication"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.kafka" %% "kafka" % "0.8.2.1"
+
+// set the main class for 'sbt run'
+mainClass in (Compile, run) := Some("Kafka.kafkaProducerImplementation")
+
+// set the main class for packaging the main jar
+mainClass in (Compile, packageBin) := Some("Kafka.kafkaProducerImplementation")
+
+
---
+++ src/main/Java/Kafka/kafkaProducerImplementation.java
@@ -0,0 +1,19 @@
+package Kafka;
+
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class kafkaProducerImplementation {
+
+    public static void main(String args[]){
+
+
+        System.out.println("hgee");
+
+
+    }
+}
